e8c37b9ad496868d9f323a470b63bee74965798e
Mark <mhazoldt+gh@gmail.com>
2017-08-21 15:07:35 -0700

    gabble

--- db/migration.sql
+++ db/migration.sql
@@ -1,31 +1,133 @@
+CREATE DATABASE  IF NOT EXISTS `gabble` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gabble`;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: gabble
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.2.7-MariaDB
 
-/* create gabble database */
-CREATE SCHEMA `gabble` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `gab`
+--
 
-/* create users table */
-CREATE TABLE `gabble`.`users` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(25) NOT NULL,
-  `password` BINARY(60) NOT NULL,
+DROP TABLE IF EXISTS `gab`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gab` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` int(11) NOT NULL,
+  `content` varchar(140) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC));
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/* create gab table */
-  CREATE TABLE `gabble`.`gab` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `author` INT NOT NULL,
-  `content` VARCHAR(140) NOT NULL,
+--
+-- Dumping data for table `gab`
+--
+
+LOCK TABLES `gab` WRITE;
+/*!40000 ALTER TABLE `gab` DISABLE KEYS */;
+INSERT INTO `gab` VALUES (3,3,'new content'),(5,2,'asdfasdf'),(7,2,'some content');
+/*!40000 ALTER TABLE `gab` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gab_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+  UNIQUE KEY `uq_user_gab` (`gab_id`,`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-  /* create likes table */
-  CREATE TABLE `gabble`.`likes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `gab_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  PRIMARY KEY (`id`));
+--
+-- Dumping data for table `likes`
+--
 
-/* add unique constraint for user_id and gab_id */
-ALTER TABLE gabble.likes
-ADD CONSTRAINT uq_user_gab UNIQUE(gab_id, user_id);
+LOCK TABLES `likes` WRITE;
+/*!40000 ALTER TABLE `likes` DISABLE KEYS */;
+INSERT INTO `likes` VALUES (4,3,2),(1,3,3),(10,3,4),(12,4,4),(17,5,3),(13,5,4),(14,6,4),(15,7,3);
+/*!40000 ALTER TABLE `likes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) unsigned NOT NULL,
+  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sessions`
+--
+
+LOCK TABLES `sessions` WRITE;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+INSERT INTO `sessions` VALUES ('UuQZ_P2fTiLEsl89ShOIsTahoGMN6-B3',1502907816,'{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"flash\":{\"info\":[\"success_msg\",\"error_msg\",\"error\",\"success_msg\",\"error_msg\",\"error\",\"success_msg\",\"error_msg\",\"error\",\"success_msg\",\"error_msg\",\"error\"]},\"passport\":{\"user\":{\"user_id\":2,\"username\":\"user3\"}}}');
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `password` binary(60) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `idx_users_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'user2','$2a$10$7oGK84aan4P4foRGrloEoeAuluZHuql3BO/POhzLSYenzaGkLauYe'),(2,'user3','$2a$10$v/4TKYNqLw0oVkhB0aPAce5hjnaH/K4o23/iBCtOBnawFiM9f7wLC'),(3,'user4','$2a$10$GsQuvDBPUlGpR69V7uBCJO.78DTxMLgzY8DvpH3potc3xfCVUoD5K'),(4,'user5','$2a$10$Nx4Aro6MCZ/9y2eXPCU1aOBF/QxDCAGWFILmtVXWZ24JcOaCl4HRq');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-21 15:06:56
